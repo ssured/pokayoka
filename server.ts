d503beb08295c825b76f7c34c87be36cf0f1c16d
Sjoerd de Jong <sjoerd@weett.nl>
2018-12-27 14:27:56 +0100

    support history api

--- server.ts
+++ server.ts
@@ -1,3 +1,4 @@
+import path from 'path';
 import express from 'express';
 import webpack from 'webpack';
 import webpackDevMiddleware from 'webpack-dev-middleware';
@@ -14,23 +15,43 @@ const compiler = webpack(webpackConfig);
 
 const isDevelopment = true;
 
+app.use(wsRouterFor(getWss()));
+
 if (isDevelopment) {
   // Tell express to use the webpack-dev-middleware and use the webpack.config.js
   // configuration file as a base.
-  app.use(
-    webpackDevMiddleware(compiler, {
-      // noInfo: true,
-      publicPath: webpackConfig.output!.publicPath!,
-    })
-  );
+  const devMiddleware = webpackDevMiddleware(compiler, {
+    // noInfo: true,
+    publicPath: webpackConfig.output!.publicPath!,
+  });
+  app.use((req, res, next) => {
+    const reqPath = req.url;
+    // find the file that the browser is looking for
+    const file = reqPath.split('/').pop()!;
+
+    if (file.match(/^(index\.html)|(.*\.bundle\.js)|(.*\.worker\.js)$/)) {
+      res.end(
+        devMiddleware.fileSystem.readFileSync(
+          path.join(webpackConfig.output!.path!, file)
+        )
+      );
+    } else if (file.indexOf('.') === -1) {
+      // if the url does not have an extension, assume they've navigated to something like /home and want index.html
+      res.end(
+        devMiddleware.fileSystem.readFileSync(
+          path.join(webpackConfig.output!.path!, 'index.html')
+        )
+      );
+    } else {
+      next();
+    }
+  });
   app.use(
     webpackHotMiddleware(compiler, {
       reload: true,
     })
   );
 }
-
-app.use(wsRouterFor(getWss()));
 
 // Serve the files on port 3000.
 app.listen(3000, () => {
