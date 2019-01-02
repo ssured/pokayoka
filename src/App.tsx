be9ce215fe7db80df02f39c28d48b95a2069ed59
Sander Soeteman <sander@auger.nl>
2019-01-02 19:58:00 +0100

    removed old nlsfb stuff

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,6 @@ import { Home } from './routes/Home';
 import { User } from './routes/User';
 import { Project } from './routes/Project';
 import { Debug } from './routes/Debug';
-import { Nlsfb } from './routes/Nlsfb';
-import { NlsfbElement } from './routes/NlsfbElement';
 
 type Props = {};
 
@@ -14,9 +12,7 @@ export const App: React.SFC<Props> = ({}) => (
   <Router>
     <Home path="/" />
     <Debug path="debug" />
-    <Nlsfb path="nlsfb">
-      <NlsfbElement path=":elementId" />
-    </Nlsfb>
+
     {/* <User path=":userId">
       <Project path=":projectId" />
     </User> */}
