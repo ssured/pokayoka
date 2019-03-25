70b4dc76951056b0f31d2d3b37d5a9e82120c9aa
Sjoerd de Jong <sjoerd@weett.nl>
2019-03-25 10:10:51 +0100

    remember the subdomain

--- server/expose-localtunnel.ts
+++ server/expose-localtunnel.ts
@@ -1,16 +1,36 @@
 import localtunnel, { Tunnel } from 'localtunnel';
 import { Express } from 'express';
 import pify from 'pify';
-import console = require('console');
+import { readFileSync, writeFileSync } from 'fs';
+import path from 'path';
+
+const SUBDOMAIN_FILE = 'localtunnel-name.txt';
+
+const subdomainFile = path.join(__dirname, SUBDOMAIN_FILE);
+const subdomain: string | undefined = (() => {
+  try {
+    return readFileSync(subdomainFile, 'utf-8');
+  } catch (e) {
+    return undefined;
+  }
+})();
 
 let tunnel: Tunnel;
+
+function writeSubdomainFromUrl(url: string) {
+  const matches = url.match(/^https?:\/\/(.*)\.localtunnel\.me$/);
+  if (matches) {
+    writeFileSync(subdomainFile, matches[1], 'utf-8');
+  }
+}
 
 export function addTunnel(app: Express, port: number) {
   app.get('/tunnel', async (req, res) => {
     try {
       if (!tunnel) {
-        tunnel = await pify(localtunnel)(port);
+        tunnel = await pify(localtunnel)(port, { subdomain });
         console.log('Created tunnel: ', tunnel.url);
+        writeSubdomainFromUrl(tunnel.url!);
         //   tunnel!.on('request', (info) => console.log('tunnel', info))
         process.on('SIGINT', () => {
           tunnel.close();
