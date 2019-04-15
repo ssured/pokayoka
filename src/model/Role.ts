0f5adb97a7e567ac51bd6728bd78c35cfc2fead0
Sjoerd de Jong <sjoerd@weett.nl>
2019-04-15 10:55:06 +0200

    formik

--- src/model/Role.ts
+++ src/model/Role.ts
@@ -1,10 +1,9 @@
 import { One } from './base';
 
 declare global {
-  type Role = {
+  type Role = Partial<{
     '@type': 'Role';
     identifier: string;
-  } & Partial<{
     /**
      * A role played, performed or filled by a person or organization. For example, the team of creators for a comic book might fill the roles named 'inker', 'penciller', and 'letterer'; or an athlete in a SportsTeam might play in the position named 'Quarterback'.
      */
--- src/routes/Paged/AddContactPerson.tsx
+++ src/routes/Paged/AddContactPerson.tsx
@@ -1,164 +1,160 @@
 import { RouteComponentProps } from '@reach/router';
 import { observer } from 'mobx-react-lite';
-import React from 'react';
+import React, { useState } from 'react';
 import { Page, PageTitle } from '../../components/Page/Page';
 import {
   Box,
   Button,
   FormField,
   TextInput,
-  FormFieldProps,
   TextInputProps,
   Grid,
   Heading,
   Select,
 } from 'grommet';
 import { Save } from 'grommet-icons';
-import {
-  Formik,
-  FormikActions,
-  FormikProps,
-  Form,
-  Field,
-  FieldProps,
-} from 'formik';
-import useFormal from '@kevinwolf/formal-web';
+import { Formik, FormikProps, useField } from 'formik';
 import * as yup from 'yup';
 
-const schema = yup.object().shape({
-  givenName: yup.string(),
-  additionalName: yup.string(),
-  familyName: yup.string().required(),
-  email: yup.string().email(),
-  telephone: yup.string(),
-  description: yup.string(),
-  role: yup.string().required(),
+const validationSchema = yup.object<Role>().shape({
+  roleName: yup.string().required(),
+  member: yup.object().shape({
+    givenName: yup.string(),
+    additionalName: yup.string(),
+    familyName: yup.string().required(),
+    email: yup.string().email(),
+    telephone: yup.string(),
+    description: yup.string(),
+    // role: yup.string().required(),
+  }),
 });
 
-const initialValues: Person = {
-  givenName: 'Tony',
-  familyName: 'Stark',
-  email: 'ironman@avengers.io',
+type MyFormValues = Role;
+
+const initialValues: Role = {
+  roleName: 'uitvoerder',
+  // member: {
+  //   givenName: 'Tony',
+  //   familyName: 'Stark',
+  //   email: 'ironman@avengers.io',
+  // },
 };
 
-const Field: React.FunctionComponent<
-  TextInputProps & { label: string; error?: string }
-> = ({ label, error, placeholder, ...props }) => {
+const TextField: React.FunctionComponent<
+  TextInputProps & { name: string; label: string; placeholder?: string }
+> = ({ label, ...props }) => {
+  const [field, meta] = useField(props.name);
   return (
-    <FormField {...{ label, error }}>
-      <TextInput {...props} />
-    </FormField>
+    <>
+      <FormField {...{ label, error: meta.touched ? meta.error : undefined }}>
+        <TextInput {...{ ...field, value: field.value || '' }} {...props} />
+      </FormField>
+    </>
   );
 };
+
 export const AddContactPerson: React.FunctionComponent<
   RouteComponentProps<{}> & {}
 > = observer(({}) => {
-  const formal = useFormal(initialValues, {
-    schema,
-    onSubmit: values => console.log('Your values are:', values),
-  });
+  const [submitted, setSubmitted] = useState(false);
 
   return (
     <PageTitle title="Contactpersoon toevoegen" href={`./add-contact`}>
       <Page>
         <Formik
-          initialValues={{ firstName: '' }}
-          onSubmit={(
-            values: MyFormValues,
-            actions: FormikActions<MyFormValues>
-          ) => {
-            console.log({ values, actions });
+          {...{ initialValues, validationSchema }}
+          validateOnBlur={submitted}
+          validateOnChange={submitted}
+          onSubmit={(values, helpers) => {
+            console.log({ values, actions: helpers });
             alert(JSON.stringify(values, null, 2));
-            actions.setSubmitting(false);
+            helpers.setSubmitting(false);
           }}
-          render={(formikBag: FormikProps<MyFormValues>) => <Form />}
+          render={({
+            handleSubmit,
+            isSubmitting,
+          }: FormikProps<MyFormValues>) => (
+            <form
+              onSubmit={e => {
+                e.preventDefault();
+                setSubmitted(true);
+                handleSubmit();
+              }}
+            >
+              <Grid
+                fill
+                rows={['auto', 'auto', 'auto']}
+                columns={['auto', 'auto']}
+                areas={[
+                  // { name: 'header', start: [0, 0], end: [1, 0] },
+                  { name: 'buttons', start: [0, 0], end: [1, 0] },
+                  { name: 'search', start: [0, 1], end: [0, 1] },
+                  { name: 'left', start: [0, 2], end: [0, 2] },
+                  { name: 'right', start: [1, 2], end: [1, 2] },
+                ]}
+                gap="medium"
+              >
+                <Box
+                  justify="end"
+                  direction="row"
+                  gap="medium"
+                  gridArea="buttons"
+                >
+                  <Button
+                    label="Annuleren"
+                    onClick={() => window.history.back()}
+                    disabled={isSubmitting}
+                  />
+                  <Button
+                    primary
+                    type="submit"
+                    icon={<Save />}
+                    label="Opslaan"
+                    disabled={isSubmitting}
+                  />
+                </Box>
+
+                <Box gridArea="search">
+                  <Select
+                    placeholder="Zoek persoon op naam"
+                    options={[]}
+                    onSearch={() => {}}
+                  />
+                </Box>
+
+                <Box gridArea="left">
+                  <Heading level="3">Personalia</Heading>
+
+                  <TextField label="Voornaam" name="member.givenName" />
+                  <TextField
+                    label="Tussenvoegsel"
+                    name="member.additionalName"
+                  />
+                  <TextField label="Achternaam" name="member.familyName" />
+                  <TextField label="Email" name="member.email" />
+                </Box>
+
+                <Box gridArea="right">
+                  <Heading level="3">Rol</Heading>
+                  <TextField label="Rol" name="roleName" />
+                </Box>
+              </Grid>
+              {/* <Field name="lastName">
+            {({
+              field, // { name, value, onChange, onBlur }
+              form: { touched, errors }, // also values, setXXXX, handleXXXX, dirty, isValid, status, etc.
+              meta,
+            }) => (
+              <div>
+                <input type="text" placeholder="Email" {...field} />
+                {meta.touched &&
+                  meta.error && <div className="error">{meta.error}</div>}
+              </div>
+            )}
+          </Field> */}
+            </form>
+          )}
         />
-        <form {...formal.getFormProps()}>
-          <Grid
-            fill
-            rows={['auto', 'auto', 'auto']}
-            columns={['auto', 'auto']}
-            areas={[
-              // { name: 'header', start: [0, 0], end: [1, 0] },
-              { name: 'buttons', start: [0, 0], end: [1, 0] },
-              { name: 'search', start: [0, 1], end: [0, 1] },
-              { name: 'left', start: [0, 2], end: [0, 2] },
-              { name: 'right', start: [1, 2], end: [1, 2] },
-            ]}
-            gap="medium"
-          >
-            <Box justify="end" direction="row" gap="medium" gridArea="buttons">
-              <Button label="Annuleren" onClick={() => window.history.back()} />
-              <Button
-                {...formal.getSubmitButtonProps()}
-                primary
-                icon={<Save />}
-                label="Opslaan"
-              />
-            </Box>
-
-            <Box gridArea="search">
-              <Select
-                placeholder="Zoek persoon op naam"
-                options={[]}
-                onSearch={() => {}}
-              />
-            </Box>
-
-            <Box gridArea="left">
-              <Heading level="3">Personalia</Heading>
-
-              <Field label="Voornaam" {...formal.getFieldProps('givenName')} />
-
-              <FormField
-                label="Tussenvoegsel"
-                htmlFor="additionalName"
-                error={formal.errors.additionalName}
-              >
-                <TextInput
-                  id="additionalName"
-                  placeholder=""
-                  {...formal.getFieldProps('additionalName')}
-                />
-              </FormField>
-
-              <FormField
-                label="Achternaam"
-                htmlFor="familyName"
-                error={formal.errors.familyName}
-              >
-                <TextInput
-                  id="familyName"
-                  placeholder=""
-                  {...formal.getFieldProps('familyName')}
-                />
-              </FormField>
-
-              <FormField
-                label="Email"
-                htmlFor="email"
-                error={formal.errors.email}
-              >
-                <TextInput
-                  id="email"
-                  placeholder=""
-                  {...formal.getFieldProps('email')}
-                />
-              </FormField>
-            </Box>
-
-            <Box gridArea="right">
-              <Heading level="3">Rol</Heading>
-              <FormField label="Rol" htmlFor="role" error={formal.errors.role}>
-                <TextInput
-                  id="role"
-                  placeholder=""
-                  {...formal.getFieldProps('role')}
-                />
-              </FormField>
-            </Box>
-          </Grid>
-        </form>
       </Page>
     </PageTitle>
   );
